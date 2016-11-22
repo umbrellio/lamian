e419c1e440fa590437667d404d4025dbd4b7707f
JelF <begdory4+lamian@gmail.com>
2016-11-22 10:14:04 +0300

    reproduced #6

--- spec/lamian/logger_spec.rb
+++ spec/lamian/logger_spec.rb
@@ -6,4 +6,20 @@ describe Lamian::Logger, :cool_loggers do
       expect(Lamian.dump).to eq "it's alive\n"
     end
   end
+
+  describe '#dump' do
+    specify 'with #run' do
+      Lamian.run do
+        generic_logger.info "it's alive"
+        expect(Lamian.dump).to eq "it's alive\n"
+        expect(Lamian.dump).to eq "it's alive\n"
+      end
+    end
+
+    specify 'without #run' do
+      generic_logger.info "it's alive"
+      expect { Lamian.dump }.not_to raise_error
+      expect(Lamian.dump).to be_nil
+    end
+  end
 end
