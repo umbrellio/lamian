eb50fad40a3ed9885391a51be8294806ef483b9f
Yuri Smirnov <tycooon@yandex.ru>
2018-12-25 18:54:25 +0300

    add link to raven context docs

--- lib/lamian/raven_context_extension.rb
+++ lib/lamian/raven_context_extension.rb
@@ -3,6 +3,7 @@
 # A patch for Raven::Context class
 module Lamian::RavenContextExtension
   # Adds current lamian log to the extra part of all raven events generated inside Lamian.run block
+  # @see https://www.rubydoc.info/gems/sentry-raven/0.9.2/Raven/Context#extra-instance_method
   def extra
     extra = super || {}
     extra[:lamian_log] ||= Lamian.dump(format: :txt)
