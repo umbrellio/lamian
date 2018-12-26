8d9d5eab811ffae4ddbe5e6aa11d566d5a86e32d
Yuri Smirnov <tycooon@yandex.ru>
2018-12-26 20:06:59 +0300

    improve specs

--- spec/lamian/sidekiq_raven_middleware_spec.rb
+++ spec/lamian/sidekiq_raven_middleware_spec.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 describe Lamian::SidekiqRavenMiddleware, :cool_loggers do
-  it "call Raven.extra_context with proper lamian_log" do
-    expect(Raven).to receive(:extra_context)
-
+  it "calls Raven.extra_context and adds Lamian.run" do
     middleware_call = proc do
       Lamian::SidekiqRavenMiddleware.new.call do
+        generic_logger.info "some log"
         raise "some error"
       end
     end
 
+    expect(Raven.extra_context).not_to have_key(:lamian_log)
     expect(middleware_call).to raise_error(RuntimeError, "some error")
+    expect(Raven.extra_context[:lamian_log]).to eq("some log\n")
   end
 end
