9644c1ca067060c4ee2fe0e03a29817ccd497bd8
Yuri Smirnov <tycooon@yandex.ru>
2018-12-26 20:50:55 +0300

    extract cool loggers context

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,24 +9,8 @@ Coveralls.wear!
 
 require "lamian"
 
-require_relative "support/raven"
+Dir[File.join(__dir__, "support/**/*.rb")].each { |x| require(x) }
 
 RSpec.configure do |config|
   config.order = :random
-end
-
-shared_context "cool loggers", :cool_loggers do
-  let(:generic_logger_buffer) { StringIO.new }
-  let(:generic_logger) { ::Logger.new(generic_logger_buffer) }
-
-  let(:cool_formatter) do
-    -> (_severity, _date, _progname, message) { "#{message}\n" }
-  end
-
-  before("extend generic_logger") { Lamian.extend_logger(generic_logger) }
-
-  before("stub formatter") do
-    allow(Lamian.config).to receive(:formatter).and_return(cool_formatter)
-    generic_logger.formatter = cool_formatter
-  end
 end
---
+++ spec/support/cool_loggers.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+shared_context "cool loggers", :cool_loggers do
+  let(:generic_logger_buffer) { StringIO.new }
+  let(:generic_logger) { ::Logger.new(generic_logger_buffer) }
+
+  let(:cool_formatter) do
+    -> (_severity, _date, _progname, message) { "#{message}\n" }
+  end
+
+  before("extend generic_logger") { Lamian.extend_logger(generic_logger) }
+
+  before("stub formatter") do
+    allow(Lamian.config).to receive(:formatter).and_return(cool_formatter)
+    generic_logger.formatter = cool_formatter
+  end
+end
