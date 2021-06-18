57728a750d44afb60ae48ed1b1331608395a7ced
Ivan Chernov <vanyaz158@gmail.com>
2021-06-18 20:19:03 +0300

    Cover missed branch

--- spec/lamian/sidekiq_sentry_middleware_spec.rb
+++ spec/lamian/sidekiq_sentry_middleware_spec.rb
@@ -1,16 +1,32 @@
 # frozen_string_literal: true
 
 describe Lamian::SidekiqSentryMiddleware, :cool_loggers do
-  it "calls Sentry.set_extras and adds Lamian.run" do
-    middleware_call = proc do
+  around do |example|
+    Sentry.with_scope { example.call }
+  end
+
+  let(:middleware_call) do
+    proc do
       Lamian::SidekiqSentryMiddleware.new.call do
         generic_logger.info "some log"
         raise "some error"
       end
     end
+  end
 
-    expect(Sentry.get_current_scope.extra).not_to have_key(:lamian_log)
+  it "calls Sentry.set_extras and adds Lamian.run" do
+    expect(Sentry.get_current_scope.extra).not_to include(:lamian_log)
     expect(middleware_call).to raise_error(RuntimeError, "some error")
     expect(Sentry.get_current_scope.extra[:lamian_log]).to eq("some log\n")
+  end
+
+  context "without logs" do
+    before { allow(Lamian).to receive(:dump_limited).and_return(nil) }
+
+    it "doesn't set extras" do
+      expect(Sentry.get_current_scope.extra).not_to include(:lamian_log)
+      expect(middleware_call).to raise_error(RuntimeError, "some error")
+      expect(Sentry.get_current_scope.extra).not_to include(:lamian_log)
+    end
   end
 end
