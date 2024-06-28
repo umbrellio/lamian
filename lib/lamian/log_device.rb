68658dba973a24e3f2275a524be5cc730e7dfc5d
Yuri Smirnov <tycoooon@gmail.com>
2024-06-28 18:17:47 +0300

    add nodoc

--- lib/lamian/log_device.rb
+++ lib/lamian/log_device.rb
@@ -1,20 +1,19 @@
 # frozen_string_literal: true
 
 module Lamian
-  # @api private
-  class LogDevice
+  class LogDevice # :nodoc:
     def initialize(size = Lamian.config.max_log_lines)
       self.size = size
       self.lines = []
     end
 
-    def write(string)
+    def write(string) # :nodoc:
       lines << string
       lines.shift if lines.size > size
       true
     end
 
-    def string
+    def string # :nodoc:
       lines.join
     end
 
